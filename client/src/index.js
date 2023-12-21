cfb6564c4846be713eb6cfc076db2d620f68b12a
hirakawatsuneteru <hirakawatsuneteru@gmail.com>
2023-12-21 02:22:03 +0100

    complete lift 2

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
+const client = new ApolloClient({
+  uri: 'http://localhost:4000', 
+  cache: new InMemoryCache(),
+})
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
